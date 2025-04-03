3d38c8e119f3de1a65badd0c763a62fb0dfcf334
arghyaadglobal360 <arghya.khanra@adglobal360.com>
2025-04-03 15:34:00 +0530

    Update test_python_code.py

--- test_python_code.py
+++ test_python_code.py
@@ -1,6 +1,9 @@
 def greet(name):
     return f"Hello, {name}! Welcome to Git testing."
-
+def test_1(x):
+    return x*x
 if __name__ == "__main__":
     user_name = input("Enter your name: ")
+    x=input()
     print(greet(user_name))
+    print(test_1(x))
